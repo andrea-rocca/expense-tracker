04bfd344f7423c40e42ca7eb10c4ca6128836a00
Andrea <andrearocca83@gmail.com>
2022-01-07 18:04:01 +0100

    static components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ExpenseItem from './components/Expenses/ExpenseItem';
+import Card from './components/UI/Card';
 
 class App extends Component {
+
   render() {
+
+    const expenses = [
+      {
+        id: 'e1',
+        title: 'Toilet Paper',
+        amount: 4.5,
+        date: new Date(2021,2,28)
+      },
+      {
+        id: 'e2',
+        title: 'New TV',
+        amount: 120,
+        date: new Date(2021,6,26)
+      },
+      {
+        id: 'e3',
+        title: 'Car Insurance',
+        amount: 294.67,
+        date: new Date(2021,2,28)
+      },
+      {
+        id: 'e4',
+        title: 'New Desk',
+        amount: 450,
+        date: new Date(2021,2,28)
+      }
+    ];
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Card className="App expenses">
+          <h2>EXP TRACKER</h2>
+          <ExpenseItem 
+              title={expenses[0].title} 
+              amount={expenses[0].amount} 
+              date={expenses[0].date}>
+          </ExpenseItem>
+          <ExpenseItem 
+              title={expenses[1].title} 
+              amount={expenses[1].amount} 
+              date={expenses[1].date}>
+          </ExpenseItem>
+      </Card>
     );
   }
 }
+
 
 export default App;
